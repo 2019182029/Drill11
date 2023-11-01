d2dc88fa31ec111fd9b17642613fd6b0c0167873
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-11-01 21:07:16 +0900

    bird 움직임을 프레임 시간에 의한 연산으로 변경

--- bird.py
+++ bird.py
@@ -1,5 +1,17 @@
 from pico2d import *
 import random
+import game_framework
+
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 5.0
+RUN_SPEED_MPH = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_SPH = (RUN_SPEED_MPH / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_SPH * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 5
+FRAMES_PER_TIME = ACTION_PER_TIME * FRAMES_PER_ACTION
 
 
 class Bird:
@@ -12,8 +24,8 @@ class Bird:
         self.image = load_image('bird_animation.png')
 
     def update(self):
-        self.x += self.dir
-        self.frame = (self.frame + 1) % 5
+        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time
+        self.frame = (self.frame + FRAMES_PER_TIME * game_framework.frame_time) % 5
 
         if self.x > 1500:
             self.dir = -5
@@ -32,10 +44,10 @@ class Bird:
 
     def draw(self):
         if self.face_dir == 1:
-            self.image.clip_draw(self.frame * (918 // 5) - 2, self.action * (506 // 3) - 2, (918 // 5), (506 // 3),
+            self.image.clip_draw(int(self.frame) * (918 // 5) - 2, self.action * (506 // 3) - 2, (918 // 5), (506 // 3),
                                  self.x,
                                  self.y, 100, 100)
         elif self.face_dir == -1:
-            self.image.clip_composite_draw(self.frame * (918 // 5) - 2, self.action * (506 // 3) - 2, (918 // 5),
+            self.image.clip_composite_draw(int(self.frame) * (918 // 5) - 2, self.action * (506 // 3) - 2, (918 // 5),
                                            (506 // 3), 0, 'h', self.x,
                                            self.y, 100, 100)
