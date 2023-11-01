e7d2312d66265de2968e3f42ae804f47646a41c7
2019182029_이원준 <S159753S@tukorea.ac.kr>
2023-11-01 21:01:49 +0900

    bird 좌우 왕복 이동 구현

--- bird.py
+++ bird.py
@@ -1,28 +1,41 @@
 from pico2d import *
 import random
 
+
 class Bird:
     def __init__(self):
-        self.x, self.y = random.randint(100, 1500), 400
+        self.x, self.y = random.randint(100, 1500), random.randint(400, 500)
         self.frame = random.randint(0, 4)
         self.action = 2
-        self.face_dir = 1
-        self.dir = 0
+        self.face_dir = random.choice([1, -1])
+        self.dir = 5 if self.face_dir == 1 else -5
         self.image = load_image('bird_animation.png')
 
-
     def update(self):
+        self.x += self.dir
         self.frame = (self.frame + 1) % 5
 
+        if self.x > 1500:
+            self.dir = -5
+            self.face_dir = -1
+        elif self.x < 100:
+            self.dir = 5
+            self.face_dir = 1
+
         if self.action == 0 and self.frame == 3:
-            self.action = 2
-            self.frame = 0
+            self.action, self.frame = 2, 0
         elif self.frame == 4:
-            self.action -= 1
-            self.frame = 0
+            self.action, self.frame = self.action - 1, 0
 
     def handle_event(self, event):
         pass
 
     def draw(self):
-        self.image.clip_draw(self.frame * (918 // 5) - 2, self.action * (506 // 3) - 2, (918 // 5), (506 // 3), self.x, self.y, 100, 100)
+        if self.face_dir == 1:
+            self.image.clip_draw(self.frame * (918 // 5) - 2, self.action * (506 // 3) - 2, (918 // 5), (506 // 3),
+                                 self.x,
+                                 self.y, 100, 100)
+        elif self.face_dir == -1:
+            self.image.clip_composite_draw(self.frame * (918 // 5) - 2, self.action * (506 // 3) - 2, (918 // 5),
+                                           (506 // 3), 0, 'h', self.x,
+                                           self.y, 100, 100)
